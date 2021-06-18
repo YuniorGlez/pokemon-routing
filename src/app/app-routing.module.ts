a6c388469ee1b23a84fa6095a96bff1092ab6f1c
Yunior Gonzalez <yunior@squaads.com>
2021-06-18 07:42:31 +0100

    Preparando las rutas

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,18 @@
 import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
+import { HomeComponent } from './components/home/home.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+  {
+    path: '',
+    component : HomeComponent
+  },
+  {
+    path: 'pokemon/:id',
+    component : PokemonDetailsComponent
+  }
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,14 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { PokemonDetailsComponent } from './components/pokemon-details/pokemon-details.component';
+import { HomeComponent } from './components/home/home.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    PokemonDetailsComponent,
+    HomeComponent
   ],
   imports: [
     BrowserModule,
